55428d4638ddf288cdcd51c3deb2bb164167ccb0
Joe Schoonover <joe@fluidnumerics.com>
2021-04-14 17:20:41 +0000

    Modify install script for nvidia platform build

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 module load gcc/10.2.0
 
 make clean
-sed -i 's/GPU=0/GPU=1/g' Makefile
+#sed -i 's/GPU=0/GPU=1/g' Makefile
 #sed -i 's/OPENCV=0/OPENCV=1/g' Makefile
-
-export HIP_PLATFORM=hcc
-export HIP_GPU=sm_70
+export GPU_PLATFORM=nvcc
+export HIP_PLATFORM=nvcc
+export HIP_RUNTIME=NVCC
+#export HCC_AMDGPU_TARGET=gfx906
+#export HIP_GPU=sm_70
 
 make
 
